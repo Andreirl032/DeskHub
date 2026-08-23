2d7828e17197c98083723b074b5708e969bc04bf
Andreirl032 <andrei.ramos.lopes@gmail.com>
2026-08-23 16:32:13 -0300

    consulta criar organização, buscar organização, corrigido erro .env, criado cargo OWNER no schema

--- backend/src/organization/organization.service.ts
+++ backend/src/organization/organization.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { UpdateOrganizationDto } from './dto/update-organization.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class OrganizationService {
-  create(createOrganizationDto: CreateOrganizationDto) {
-    return 'This action adds a new organization';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createOrganizationDto: CreateOrganizationDto) {
+    const name = createOrganizationDto.name;
+    const createOrganizationDb = await this.prisma.organization.create({
+      data: {
+        name: name,
+      },
+    });
+    return createOrganizationDb;
   }
 
   findAll() {
     return `This action returns all organization`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} organization`;
+  async findOne(id: string) {
+    const findOneDb = await this.prisma.organization.findUnique({
+      where: { id: id },
+    });
+    return findOneDb;
   }
 
-  update(id: number, updateOrganizationDto: UpdateOrganizationDto) {
+  update(id: string, updateOrganizationDto: UpdateOrganizationDto) {
     return `This action updates a #${id} organization`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} organization`;
   }
 }
