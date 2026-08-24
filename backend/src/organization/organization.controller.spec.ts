2457465fd636edbed7938e6d4ff0c21431c7f0e1
Andreirl032 <andrei.ramos.lopes@gmail.com>
2026-08-24 18:36:17 -0300

    criados testes organization.controller

--- backend/src/organization/organization.controller.spec.ts
+++ backend/src/organization/organization.controller.spec.ts
@@ -3,13 +3,41 @@ import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
 import { PrismaService } from '../prisma/prisma.service';
 
+const mockPrisma = {
+  organization: {
+    create: jest.fn().mockResolvedValue({
+      id: '123',
+      name: 'organizationTest',
+      createdAt: new Date(),
+    }),
+    findUnique: jest.fn().mockResolvedValue({
+      id: 'jcyddhmrzmd1m9iyf1k5zwze',
+      name: 'Empresa',
+      createdAt: new Date(),
+    }),
+    update: jest.fn().mockResolvedValue({
+      id: 'jcyddhmrzmd1m9iyf1k5zwze',
+      name: 'Enterprise',
+      createdAt: new Date(),
+    }),
+    delete: jest.fn().mockResolvedValue({
+      id: 'jcyddhmrzmd1m9iyf1k5zwze',
+      name: 'Empresa',
+      createdAt: new Date(),
+    }),
+  },
+};
+
 describe('OrganizationController', () => {
   let controller: OrganizationController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrganizationController],
-      providers: [OrganizationService, PrismaService],
+      providers: [
+        OrganizationService,
+        { provide: PrismaService, useValue: mockPrisma },
+      ],
     }).compile();
 
     controller = module.get<OrganizationController>(OrganizationController);
@@ -17,5 +45,30 @@ describe('OrganizationController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('deve criar uma organização', async () => {
+    const resultado = await controller.create({ name: 'organizationTest' });
+    expect(resultado).toBeDefined();
+  });
+
+  it('deve atualizar uma organização', async () => {
+    const resultado = await controller.update('jcyddhmrzmd1m9iyf1k5zwze', {
+      name: 'Enterprise',
+    });
+    expect(resultado).toBeDefined();
+  });
+
+  it('deve achar uma organização', async () => {
+    const resultado = await controller.findOne('jcyddhmrzmd1m9iyf1k5zwze');
+    expect(resultado).toMatchObject({
+      id: 'jcyddhmrzmd1m9iyf1k5zwze',
+      name: 'Empresa',
+    });
+  });
+
+  it('deve apagar uma organização', async () => {
+    const resultado = await controller.remove('jcyddhmrzmd1m9iyf1k5zwze');
+    expect(resultado).toBeDefined();
   });
 });
